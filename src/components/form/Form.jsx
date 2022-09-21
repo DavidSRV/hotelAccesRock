51c2b08be82b149a6c73d0cce0c39957e003f769
DavidSRV <david.ruizvargas@hotmail.com>
2022-09-20 20:22:42 -0500

    Se agrega servicios y pages

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,8 +1,35 @@
+import { useState } from "react";
+import { ServicioReserva } from "../../services/ServicioReserva";
+
 export default function Form() {
+  const [entrada, setEntrada] = useState();
+  const [salida, setSalida] = useState();
+  const [numAdultos, setNumAdultos] = useState();
+  const [numBendi, setNumBendi] = useState();
+
+  const EnvioForm = (e) => {
+    e.preventDefault();
+    let data = {
+      idHabitacion: "6321ef745a1931ff38e7c2c3",
+      fechaEntrada: entrada,
+      fechaSalida: salida,
+      numeroNinos: numBendi,
+      numeroAdultos: numAdultos,
+    };
+  };
+
   return (
-    <div >
-      <form className="w-50 mt-5"
-        style={{ background: "black", padding: "30px", borderRadius: "20px", textAlign:'center', color:'white' }}
+    <div>
+      <form
+        className="w-50 mt-5"
+        style={{
+          background: "black",
+          padding: "30px",
+          borderRadius: "20px",
+          textAlign: "center",
+          color: "white",
+        }}
+        onSubmit={EnvioForm}
       >
         <div class="mb-3">
           <label for="exampleInputEmail1" class="form-label">
@@ -13,6 +40,10 @@ export default function Form() {
             class="form-control"
             id="exampleInputEmail1"
             aria-describedby="emailHelp"
+            onChange={(e) => {
+              setEntrada(e.target.value);
+            }}
+            value={entrada}
           />
         </div>
         <div class="mb-3">
@@ -24,6 +55,10 @@ export default function Form() {
             class="form-control"
             id="exampleInputEmail1"
             aria-describedby="emailHelp"
+            onChange={(e) => {
+              setSalida(e.target.value);
+            }}
+            value={salida}
           />
         </div>
         <div class="mb-3">
@@ -34,7 +69,13 @@ export default function Form() {
             type="number"
             class="form-control"
             id="exampleInputEmail1"
+            min={1}
+            max={3}
             aria-describedby="emailHelp"
+            onChange={(e) => {
+              setNumAdultos(e.target.value);
+            }}
+            value={numAdultos}
           />
         </div>
         <div class="mb-3">
@@ -45,6 +86,10 @@ export default function Form() {
             type="number"
             class="form-control"
             id="exampleInputPassword1"
+            onChange={(e) => {
+              setNumBendi(e.target.value);
+            }}
+            value={numBendi}
           />
         </div>
 
--- src/pages/habitacion/habitacion.jsx
+++ src/pages/habitacion/habitacion.jsx
@@ -1,3 +1,40 @@
+import { ServicioHabitacion } from "../../services/ServicioHabitacion";
+import { useEffect, useState } from "react";
+
 export default function Habitacion() {
-  return <div></div>;
+  const [estadoCarga, setEstadoCarga] = useState(true);
+  const [habitaciones, setHabitacion] = useState(null);
+
+  useEffect(() => {
+    ServicioHabitacion().then((res) => {
+      setHabitacion(res);
+      setEstadoCarga(false);
+    });
+  }, []);
+
+  if (estadoCarga) {
+
+    return(
+      <>
+      <h1>Estoy Trabajando...</h1>
+    </>
+    );
+   
+  }else {
+    return (
+        <div>
+           {/* se accede a la logica con las {}, recorrer el json*/}
+            { habitaciones.datos.map(function(habitacion){
+                return (
+                    <div>
+                        <h1>{habitacion.nombre}</h1>
+                        <img src={habitacion.imagenes[0]}></img>
+                    </div>
+                )
+            })
+            } 
+        </div>
+    )
+  }  
 }
+
